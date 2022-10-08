4709280a39f8d6f82c5b8359108fd14f8e1879d4
miaoyu <miaoyu2009@qq.com>
2022-10-08 09:33:19 +0800

    websocket

--- nodejs/day01/25websocket/server.js
+++ nodejs/day01/25websocket/server.js
@@ -3,11 +3,21 @@ const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
-
+const { Server } = require("socket.io");
+const io = new Server(server);
 
 // 2. 静态文件夹，用于访问
 app.use("/", express.static(__dirname + "/public"));
 
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("chat message", (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
 
 server.listen(3000, () => {
   console.log("listening on *:3000");
