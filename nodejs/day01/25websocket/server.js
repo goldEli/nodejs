e1bbc79a2aa13466d80dfbc244ea070241d302e7
miaoyu <miaoyu2009@qq.com>
2022-10-08 09:25:13 +0800

    static server

--- nodejs/day01/25websocket/server.js
+++ nodejs/day01/25websocket/server.js
@@ -1,12 +1,12 @@
-/* 
-1. express 创建服务
-*/
-
+// 1. express 创建服务
 const express = require("express");
 const app = express();
 const http = require("http");
 const server = http.createServer(app);
 
+
+// 2. 静态文件夹，用于访问
+app.use("/", express.static(__dirname + "/public"));
 
 
 server.listen(3000, () => {
