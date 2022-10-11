e6cfae03813f56ed9f01db2aed2386a303ff93d6
miaoyu <miaoyu2009@qq.com>
2022-10-11 15:20:46 +0800

    param

--- vue/50/10router/src/views/ApiView.vue
+++ vue/50/10router/src/views/ApiView.vue
@@ -1,5 +1,15 @@
 <template>
-    <div>
-        <h1>这是api</h1>
-    </div>
+  <div>
+    <h1>这是api</h1>
+    <p>id: {{id}}</p>
+  </div>
 </template>
+<script>
+export default {
+  computed: {
+    id: function() {
+      return this.$route.query.id;
+    }
+  }
+};
+</script>
