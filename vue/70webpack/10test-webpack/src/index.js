128144ba9b620285441def797c9929ae38722a13
miaoyu <miaoyu2009@qq.com>
2022-10-14 16:06:13 +0800

    webpack

---
+++ vue/70webpack/10test-webpack/src/index.js
@@ -0,0 +1,17 @@
+// import _ from "lodash"
+import { sum } from "./a";
+const a = 1;
+const b = 2;
+console.log(sum(a, b));
+
+// function component() {
+
+//   var element = document.createElement("div");
+
+//   // Lodash（目前通过一个 script 脚本引入）对于执行这一行是必需的
+//   element.innerHTML = _.join(["Hello", "webpack"], "+");
+
+//   return element;
+// }
+
+// document.body.appendChild(component());
